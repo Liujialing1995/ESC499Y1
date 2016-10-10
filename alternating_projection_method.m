9687279680c8f0b41f34b63aee20572f7bd34b14
Jianwei Sun <jian58sun@gmail.com>
2016-10-10 02:27:23 -0400

    draft of a working projection method with fourier series

--- alternating_projection_method.m
+++ alternating_projection_method.m
@@ -8,22 +8,36 @@ lambda = 2;
 d = lambda / 2;
 beta = 2*pi*d / lambda;
 
-N = 5;
+N = 6;
 
-number_samples = 1000;
+number_samples = 10000;
 
 %phi = linspace(0, 2*pi, number_samples);
 %theta = beta*d*cos(phi);
 theta = linspace(-beta*d,beta*d, number_samples);
 
 %series_coefficients = ones(N,1);
-series_coefficients = zeros(N,1);
-series_coefficients(1) = 1; 
-series_coefficients(2) = exp(1j*pi/3); 
-series_coefficients(3) = exp(1j*2*pi/3);
-series_coefficients(4) = exp(1j*3*pi/3); 
-series_coefficients(5) = exp(1j*4*pi/3);
+% series_coefficients = zeros(N,1);
+series_coefficients = [1;1;1;1;1;1];
+direction = pi/2;
+series_coefficients = [1; ...
+                       exp(1j*-beta*d*cos(direction)); ...
+                       exp(1j*2*-beta*d*cos(direction)); ...
+                       exp(1j*3*-beta*d*cos(direction)); ...
+                       exp(1j*4*-beta*d*cos(direction)); ...
+                       exp(1j*5*-beta*d*cos(direction))];
 
+%Input requirements for the MASK
+MASK_L = zeros(1, number_samples);
+MASK_H = zeros(1, number_samples);
+
+
+MASK_H(1:number_samples/2) = 0.3;
+MASK_H(number_samples/2:end) = 1;
+MASK_L(number_samples/2 + number_samples/8:number_samples/2 + number_samples/10 + number_samples/10) = 0.9;
+
+MASK_H = fliplr(MASK_H);
+MASK_L = fliplr(MASK_L);
 
 A = zeros(1,number_samples);
 for i = 0:(N-1)
@@ -34,8 +48,61 @@ end
 A = A/N;
 
 figure;
-plot(theta,abs(A),'r');
+plot(fliplr(acos(theta/(beta*d))),fliplr(abs(A)),'r');
 hold on;
+plot(fliplr(acos(theta/(beta*d))),fliplr(MASK_H),'k');
+hold on;
+plot(fliplr(acos(theta/(beta*d))),fliplr(MASK_L),'k');
+hold on;
+axis([0 pi 0 1]);
+
+%% Method to iteratively generate the array factor
+
+iterations_limit = 1000;
+iterations_count = 1;
+valid_yet = false;
+
+% zeroth_oldest = zeros(1,number_samples);
+% first_oldest = zeros(1,number_samples);
+% second_oldest = zeros(1,number_samples);
+
+Derived_AF = A;
+while(~valid_yet&&(iterations_count < iterations_limit))
+    %Check for satisfying mask requirements
+    if((any(abs(Derived_AF) > MASK_H) == 0)&&(any(abs(Derived_AF) < MASK_L) == 0))
+        valid_yet = true;
+    end
+    
+    
+    Derived_AF(abs(Derived_AF) > MASK_H) = MASK_H(abs(Derived_AF) > MASK_H);
+    Derived_AF(abs(Derived_AF) < MASK_L) = MASK_L(abs(Derived_AF) < MASK_L);
+    % Map to N series and plot
+    for i = 0:(N-1)
+        integrand = Derived_AF.*exp(-1j*i*theta);
+        integral_sum = 0;
+        for m = 1:number_samples-1
+            integral_sum = integral_sum + beta*d/number_samples*(integrand(m)+integrand(m+1));
+        end
+        series_coefficients(i+1) = integral_sum*N/(2*beta*d);
+    end
+    Derived_AF = zeros(1,number_samples);
+    for i = 0:(N-1)
+        % Computes the array factor for each sample point
+        Derived_AF = Derived_AF + series_coefficients(i+1)*exp(1j*i*theta);
+    end
+    Derived_AF = Derived_AF/N;
+    
+    iterations_count = iterations_count + 1;
+    if(mod(iterations_count,100)==0)
+        fprintf('Elapsed iterations: %d\n',iterations_count);
+    end
+   
+    
+end
+%%
+% figure;
+% plot(theta,abs(A),'r');
+% hold on;
 
 % % Map to N series and plot
 % for i = 0:(N-1)
@@ -46,13 +113,13 @@ hold on;
 %     end
 %     series_coefficients(i+1) = integral_sum*N/(2*beta*d);
 % end
-% B = zeros(1,number_samples);
+% Derived_AF = zeros(1,number_samples);
 % for i = 0:(N-1)
 %     % Computes the array factor for each sample point
 %     %A(i) = (1/N)*transpose(series_coefficients)*exp(1j*theta(i)*transpose(linspace(0,N-1,N)));
-%     B = B + series_coefficients(i+1)*exp(1j*i*theta);
+%     Derived_AF = Derived_AF + series_coefficients(i+1)*exp(1j*i*theta);
 % end
-% B = B/N;
+% Derived_AF = Derived_AF/N;
 
 % % Map to any number of series and plot
 % N_augmented = 2*N;
@@ -71,6 +138,7 @@ hold on;
 % end
 % Derived_AF = Derived_AF/N_augmented;
 
-fprintf('Error is %d\n',sumsqr(abs(A-Derived_AF)));
+% fprintf('Error is %d\n',sumsqr(abs(A-Derived_AF)));
+fprintf('Number of iterations taken: %d\n',iterations_count);
 
-plot(theta,abs(Derived_AF),'b');
+plot(fliplr(acos(theta/(beta*d))),fliplr(abs(Derived_AF)),'b');
