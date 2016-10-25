0649ec13fbef2533caf0ea05536c0f3648e5d611
Jianwei Sun <jian58sun@gmail.com>
2016-10-25 01:13:39 -0400

    Fixed error function

--- Multibeam.m
+++ Multibeam.m
@@ -7,7 +7,7 @@ d = lambda / 2;
 % All distances are in terms of lambda
 normal_feed_distance = 10 * lambda;
 
-Sources = [0 1];
+Sources = [0 8];
 Reflect_elements = [-5/2 -4/2 -3/2 -2/2 -1/2 0 1/2 2/2 3/2 4/2 5/2];
 
 fprintf('Generating phase matrix\n');
@@ -27,6 +27,8 @@ Basis = exp(linspace(0,length(Reflect_elements)-1,length(Reflect_elements)).' * 
 
 % C_k has to be the same length as Reflect_elements
 C_k = exp(1j*zeros(length(Reflect_elements),1));
+
+multibeam_error_sumsqr_points_outside_mask(MASK_LOWER, MASK_UPPER, X_, PHASE_BASIS)
 
 %% Plot results
 fprintf('Plotting results at resolution of %d points\n',resolution);
@@ -51,30 +53,17 @@ ylabel('Pattern Magnitude');
 grid on;
 title('Multibeam Pattern Synthesis');
 
-% figure
-% 
-% %Calculates a set of colours for the variable number of motors to plot
-% plot_colors = distinguishable_colors(quantity);
-% 
-% %Generate the legend labels
-% plot_handle = zeros(quantity, 1);
-% legend_entries = cell(quantity,1);
-% for i = 1:quantity
-%     kv_string = num2str(KV(i));
-%     padding = repmat(' ', 1, 4 - length(kv_string));
-%     legend_entries(i) = cellstr(['Kv = ', padding, kv_string]);
-% end
-% 
-% %Plot the quantities
-% subplot(3,1,1);
-% for i = 1:quantity
-%     plot_handle(i) = plot(F(i,1:end-1), P_IN_F(i,1:end-1), 'Color',plot_colors(i,:));
-%     hold on;
-%     plot(F(i,end), P_IN_F(i,end), 'Marker','o', 'MarkerSize',7, 'Color',plot_colors(i,:));
-%     hold on;
-% end
-% xlabel('Force(N)');
-% ylabel('Input Power(W)');
-% grid on;
-% title('Power - Force Curves for Varying Motors for a given Propeller');
-% legend(plot_handle, legend_entries);
+figure;
+for i = 1:length(Sources)
+    plot(Phi, 0.5*mag2db(abs(PLOT_ARRAY_FACTOR(i,:))), 'Color',plot_colors(i,:));
+    hold on;
+end
+axis([0 pi -inf inf]);
+set(gca,'xtick',0:pi/8:pi);
+set(gca,'xticklabel',{'0','pi/8','pi/4','3 pi/8','pi/2','5 pi/6','3 pi/4', '7pi/8', 'pi'});
+xlabel('Far-Field Angle (Radians)');
+ylabel('Pattern Power Magnitude (dB)');
+grid on;
+title('Multibeam Pattern Synthesis');
+
+
--- multibeam_error_sumsqr_points_outside_mask.m
+++ multibeam_error_sumsqr_points_outside_mask.m
@@ -1,8 +1,15 @@
-function total_error = multibeam_error_sumsqr_points_outside_mask(Series_coefficients, Basis, Mask_upper, Mask_lower)
+function total_error = multibeam_error_sumsqr_points_outside_mask(MASK_LOWER, MASK_UPPER, X_, PHASE, BASIS)
 % Calculates the error as the sum of squares of difference of squares of
 % array factor points outside of either mask
-    Array_factor = Series_coefficients.' * Basis;
-    total_error = (sumsqr(abs(Array_factor(abs(Array_factor) < Mask_lower)).^2 - abs(Mask_lower(abs(Array_factor) < Mask_lower)).^2) + ...
-            sumsqr(abs(Array_factor(abs(Array_factor) > Mask_upper)).^2 - abs(Mask_upper(abs(Array_factor) > Mask_upper)).^2));
+%     Array_factor = Series_coefficients.' * Basis ./ length(Series_coefficients);
+%     total_error = (sumsqr(abs(Array_factor(abs(Array_factor) < Mask_lower)).^2 - abs(Mask_lower(abs(Array_factor) < Mask_lower)).^2) + ...
+%             sumsqr(abs(Array_factor(abs(Array_factor) > Mask_upper)).^2 - abs(Mask_upper(abs(Array_factor) > Mask_upper)).^2));
+
+%     AF = abs(X_.' * PHASE_BASIS);
+    AF = ones(length(X_),1)*X_.*PHASE*BASIS;
+    UPPER_EXCEED = AF > MASK_UPPER;
+    LOWER_EXCEED = AF < MASK_LOWER;
+    total_error = sumsqr(AF(UPPER_EXCEED) - MASK_UPPER(UPPER_EXCEED)) + ...
+                  sumsqr(MASK_LOWER(LOWER_EXCEED) - AF(LOWER_EXCEED));
 end
 
